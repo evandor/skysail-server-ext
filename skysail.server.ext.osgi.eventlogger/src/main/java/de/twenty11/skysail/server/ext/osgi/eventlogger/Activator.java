c92613f8b939e1ae3f28bdc4553b0495cb6d353e
Carsten Gräf <evandor@gmail.com>
2012-02-05 09:09:40 +0100

    initial

---
+++ skysail.server.ext.osgi.eventlogger/src/main/java/de/twenty11/skysail/server/ext/osgi/eventlogger/Activator.java
@@ -0,0 +1,61 @@
+/**
+ *  Copyright 2011 Carsten Gräf
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ */
+
+package de.twenty11.skysail.server.ext.osgi.eventlogger;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The bundles activator, taking care of creating a new restlet component,
+ * application and url mapping listener.
+ * 
+ * @author carsten
+ * 
+ */
+public class Activator implements BundleActivator {
+
+    /**
+     * bundles life cycle start method, creating a new restlet component, a
+     * restlet application and the urlMapping listener.
+     * 
+     * @param context
+     *            the osgi bundle context
+     * @throws Exception
+     *             starting the component may trigger an Exception
+     * 
+     */
+    public final void start(final BundleContext context) throws Exception {
+
+    }
+    
+  
+
+    /**
+     * stop lifecycle method, removing the urlMapping listener and stopping the
+     * component.
+     * 
+     * @param context
+     *            the bundleContext
+     * @throws Exception
+     *             stopping the component might trigger an exception
+     * 
+     */
+    public final void stop(final BundleContext context) throws Exception {
+    }
+
+}
