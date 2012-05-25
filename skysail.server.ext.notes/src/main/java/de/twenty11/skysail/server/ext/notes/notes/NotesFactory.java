af6cb4da11c97e9d987dcebe9262cde1df70f51b
Carsten Gräf <evandor@gmail.com>
2012-05-25 18:50:24 +0200

    initial

---
+++ skysail.server.ext.notes/src/main/java/de/twenty11/skysail/server/ext/notes/notes/NotesFactory.java
@@ -0,0 +1,51 @@
+/**
+ */
+package de.twenty11.skysail.server.ext.notes.notes;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see de.twenty11.skysail.server.ext.notes.notes.NotesPackage
+ * @generated
+ */
+public interface NotesFactory extends EFactory {
+    /**
+     * The singleton instance of the factory.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    NotesFactory eINSTANCE = de.twenty11.skysail.server.ext.notes.notes.impl.NotesFactoryImpl.init();
+
+    /**
+     * Returns a new object of class '<em>Note</em>'.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return a new object of class '<em>Note</em>'.
+     * @generated
+     */
+    Note createNote();
+
+    /**
+     * Returns a new object of class '<em>Folder</em>'.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return a new object of class '<em>Folder</em>'.
+     * @generated
+     */
+    Folder createFolder();
+
+    /**
+     * Returns the package supported by this factory.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return the package supported by this factory.
+     * @generated
+     */
+    NotesPackage getNotesPackage();
+
+} //NotesFactory
