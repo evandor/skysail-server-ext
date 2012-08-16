a551daf7e04772bb00941f7642c2183b0214c77b
Carsten Gräf <evandor@gmail.com>
2012-08-16 19:36:08 +0200

    wip

--- skysail.server.ext.dbviewer/etc/pax-runner/dbviewer.sh
+++ skysail.server.ext.dbviewer/etc/pax-runner/dbviewer.sh
@@ -4,7 +4,7 @@
 
 /home/carsten/install/pax-runner-1.7.6/bin/pax-run.sh \
 --clean \
---vmOptions="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 \
+--vmOptions="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 \
  -Dskysail.confDir=../../ \
  -Dfelix.fileinstall.dir=../../../target \
  -Dfelix.fileinstall.filter=skysail.*.jar|.*\\.cfg \
