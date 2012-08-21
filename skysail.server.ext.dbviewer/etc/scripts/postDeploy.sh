4232df4a62333c02c650793432f1568798d53f6c
Carsten Gräf <evandor@gmail.com>
2012-08-21 07:07:43 +0200

    adding working directory to release plugin config

--- skysail.server.ext.dbviewer/etc/scripts/postDeploy.sh
+++ skysail.server.ext.dbviewer/etc/scripts/postDeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cd /home/carsten/.hudson/jobs/skysail.server.ext.dbviewer/workspace/skysail.server.ext.dbviewer/target
 cp skysail.*[!-sources].jar /home/carsten/paxrunner/pax-runner-1.7.6/conf
 cp *.zip /var/www/skysail/
