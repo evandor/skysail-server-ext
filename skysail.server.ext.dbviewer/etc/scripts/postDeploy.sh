1b4030257d4adfb0860a27faaea46ed260b409dc
Carsten Gräf <evandor@gmail.com>
2012-08-21 08:11:02 +0200

    wip

--- skysail.server.ext.dbviewer/etc/scripts/postDeploy.sh
+++ skysail.server.ext.dbviewer/etc/scripts/postDeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/carsten/.hudson/jobs/skysail.server.ext.dbviewer/workspace/skysail.server.ext.dbviewer/target
-cp skysail.*[!-sources].jar /home/carsten/paxrunner/pax-runner-1.7.6/conf
-cp *.zip /var/www/skysail/
+### cd /home/carsten/.hudson/jobs/skysail.server.ext.dbviewer/workspace/skysail.server.ext.dbviewer/target
+cp skysail.server.ext.dbviewer/target/skysail.*[!-sources].jar /home/carsten/paxrunner/pax-runner-1.7.6/conf
+cp skysail.server.ext.dbviewer/target/*.zip /var/www/skysail/
