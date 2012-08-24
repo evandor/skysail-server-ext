744e7ea5c6e7c12a11d895b43367b82549f00923
Carsten Gräf <evandor@gmail.com>
2012-08-24 08:33:39 +0200

    wip

--- skysail.server.ext.dbviewer/etc/pax-runner/local.sh
+++ skysail.server.ext.dbviewer/etc/pax-runner/local.sh
@@ -5,11 +5,11 @@
 # You need to configure the path to your local pax-runner installation in the first line!             #
 #######################################################################################################
 
-### --log=WARN \
+### --log=WARNING \
 
-/home/carsten/install/pax-runner-1.7.6/bin/pax-debug.sh \
+/home/carsten/install/pax-runner-1.7.6/bin/pax-run.sh \
 --clean \
---log=TRACE \
+--log=WARNING \
 --vmOptions="\
  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 \
  -Dfelix.fileinstall.dir=../../../target,../config \
