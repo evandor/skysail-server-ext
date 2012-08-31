8e58284a0eff6e576727ff38b60534fb6374036f
Carsten Gräf <evandor@gmail.com>
2012-08-31 07:22:21 +0200

    wip

--- skysail.server.ext.dbviewer/etc/pax-runner/pax-debug.sh
+++ skysail.server.ext.dbviewer/etc/pax-runner/pax-debug.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -Dorg.ops4j.pax.url.mvn.repositories=https://oss.sonatype.org/content/groups/public,https://repository.apache.org/content/repositories/releases -cp .:$SCRIPTS:$SCRIPTS/pax-runner-1.7.6.jar org.ops4j.pax.runner.Run "$@"
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -Dorg.ops4j.pax.url.mvn.repositories=https://oss.sonatype.org/content/groups/public,https://repository.apache.org/content/repositories/releases,http://download.eclipse.org/rt/eclipselink/maven.repo -cp .:$SCRIPTS:$SCRIPTS/pax-runner-1.7.6.jar org.ops4j.pax.runner.Run "$@"
