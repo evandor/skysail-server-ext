c798436e349af787d3eafd3bb4f56a45ed111570
Carsten Gräf <evandor@gmail.com>
2012-09-18 08:01:14 +0200

    wip

--- skysail.server.ext.dbviewer/src/test/java/de/twenty11/skysail/server/ext/dbviewer/test/ConnectionResourceTest.java
+++ skysail.server.ext.dbviewer/src/test/java/de/twenty11/skysail/server/ext/dbviewer/test/ConnectionResourceTest.java
@@ -1,10 +1,12 @@
 package de.twenty11.skysail.server.ext.dbviewer.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
 import java.util.Map;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.junit.Before;
 import org.junit.Test;
 import org.restlet.Application;
@@ -13,9 +15,13 @@ import org.restlet.Response;
 import org.restlet.Restlet;
 import org.restlet.data.ChallengeResponse;
 import org.restlet.data.ChallengeScheme;
+import org.restlet.data.MediaType;
 import org.restlet.data.Method;
+import org.restlet.representation.Representation;
 import org.restlet.resource.ServerResource;
 
+import de.twenty11.skysail.common.grids.GridData;
+import de.twenty11.skysail.common.responses.SkysailResponse;
 import de.twenty11.skysail.server.ext.dbviewer.internal.DbViewerComponent;
 import de.twenty11.skysail.server.ext.dbviewer.internal.DbViewerUrlMapper;
 import de.twenty11.skysail.server.ext.dbviewer.internal.SkysailApplication;
@@ -26,10 +32,12 @@ public class ConnectionResourceTest {
     private Restlet inboundRoot;
     private DbViewerComponent dbViewerComponent;
 
+    /** deals with json objects */
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Before
     public void setUp() throws Exception {
         dbViewerComponent = new DbViewerComponent();
-        // skysailApplication = new SkysailApplication("/static");
         skysailApplication = dbViewerComponent.getApplication();
         Application.setCurrent(skysailApplication);
         inboundRoot = skysailApplication.getInboundRoot();
@@ -37,48 +45,41 @@ public class ConnectionResourceTest {
     }
 
     @Test
-    public void testAdd() {
+    public void testAdd() throws Exception {
 
         String resourceUri = "/dbviewer/";
         Request request = new Request(Method.GET, resourceUri);
+        Response response = handleRequest(request);
+        assertEquals(200, response.getStatus().getCode());
+        assertTrue(response.isEntityAvailable());
+        assertTrue(response.getEntity().getMediaType().equals(MediaType.APPLICATION_JSON));
+
+        Representation entity = response.getEntity();
+        // ((org.restlet.ext.jackson.JacksonRepresentation) entity).
+        SkysailResponse<GridData> response2 = mapper.readValue(entity.getText(),
+                new TypeReference<SkysailResponse<GridData>>() {
+                });
+        GridData payload = response2.getData();
+
+        System.out.println(payload);
+
+    }
+
+    private Response handleRequest(Request request) {
         ChallengeResponse authentication = new ChallengeResponse(ChallengeScheme.HTTP_BASIC, "scott", "tiger");
         request.setChallengeResponse(authentication);
         Response response = new Response(request);
         inboundRoot.handle(request, response);
-        assertEquals(200, response.getStatus().getCode());
-
-        // Client client = new Client(Protocol.HTTP);
-        // Request request = new Request(Method.GET, connectionsResource.getReference());
-        // Response response = client.handle(request);
-        // assert response.getStatus().getCode() == 200;
-        // assert response.isEntityAvailable();
-        // assert response.getEntity().getMediaType().equals(MediaType.TEXT_HTML);
-
-        // Representation.getText() empties the InputStream, so we need to store the text in a variable
-        String text;
-        try {
-            text = response.getEntity().getText();
-            // assert text.contains("search string");
-            // assert text.contains("another search string");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+        return response;
     }
 
-    private void addMappings() {
+    private void addMappings() throws ClassNotFoundException {
         Map<String, String> urlMapping = new DbViewerUrlMapper().provideUrlMapping();
         for (Map.Entry<String, String> mapping : urlMapping.entrySet()) {
-            try {
-                // logger.debug("adding new mapping from '{}' to '{}'", mapping.getKey(), mapping.getValue());
-                @SuppressWarnings("unchecked")
-                Class<? extends ServerResource> resourceClass = (Class<? extends ServerResource>) Class.forName(mapping
-                        .getValue());
-                skysailApplication.attachToRouter("" + mapping.getKey(), resourceClass);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("was not able to find class " + mapping.getValue(), e);
-            }
+            @SuppressWarnings("unchecked")
+            Class<? extends ServerResource> resourceClass = (Class<? extends ServerResource>) Class.forName(mapping
+                    .getValue());
+            skysailApplication.attachToRouter("" + mapping.getKey(), resourceClass);
         }
     }
 
