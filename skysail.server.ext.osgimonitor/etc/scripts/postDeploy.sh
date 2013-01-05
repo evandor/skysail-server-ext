9ece21c0199cbcbbde3096ed5a26853dc50cc521
evandor <evandor@gmail.com>
2013-01-05 09:38:18 +0100

    fixed path

--- skysail.server.ext.osgimonitor/etc/scripts/postDeploy.sh
+++ skysail.server.ext.osgimonitor/etc/scripts/postDeploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /home/carsten/.hudson/jobs/skysail.server.ext.osgimonitor/workspace/skysail.server.ext.osgimonitor/target
-### cp skysail.*[!-sources].jar /home/carsten/paxrunner/pax-runner-1.7.6/conf/
-cp skysail-*.zip /home/carsten/paxrunner/pax-runner-1.7.6/
+### cp skysail.*[!-sources].jar /home/carsten/paxrunner/pax-runner-1.8.5/conf/
+cp skysail-*.zip /home/carsten/paxrunner/pax-runner-1.8.5/
 cp *.zip /var/www/skysail/
 
-cd /home/carsten/paxrunner/pax-runner-1.7.6/
+cd /home/carsten/paxrunner/pax-runner-1.8.5/
 
 ./skysail-osgimonitor/stop_standalone.sh
 
