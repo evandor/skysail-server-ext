d44bdf889d8a34822dfc3edc72168079a56a5bd4
evandor <evandor@gmail.com>
2013-01-05 13:01:12 +0100

    wip

--- skysail.server.ext.osgimonitor/etc/pax-runner/rund.sh
+++ skysail.server.ext.osgimonitor/etc/pax-runner/rund.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #######################################################################################################
-# Meant to be used for testing during development. Navigate to pax-runner folder of your checked-out  #
-# skysail project and run local.sh in a terminal                                                      #
-#                                                                                                     #
-# You need to configure the path to your local pax-runner installation in the first line!             #
+# Meant to be used for continuous deployment                                                          #
 #######################################################################################################
 
+### stop existing daemon
 ../bin/pax-rund-osgimonitor.sh --stop
 
+### start up new one
 ../bin/pax-rund-osgimonitor.sh \
 --startd scan-file:file:/home/carsten/paxrunner/pax-runner-1.8.5/skysail-osgimonitor/rund.setup \
 --log=INFO \
