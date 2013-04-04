c1153ffaa5eb0cadfaf2e6106f56e6c6b84a681b
evandor <evandor@gmail.com>
2013-04-04 17:34:22 +0200

    wip

---
+++ skysail.server.ext.osgimonitor/src/test/java/de/twenty11/skysail/server/ext/osgimonitor/test/HeaderResourceTest.java
@@ -0,0 +1,67 @@
+package de.twenty11.skysail.server.ext.osgimonitor.test;
+
+import de.twenty11.skysail.common.ext.osgimonitor.BundleDescriptor;
+import de.twenty11.skysail.common.ext.osgimonitor.BundleDetails;
+import de.twenty11.skysail.common.ext.osgimonitor.HeaderDescriptor;
+import de.twenty11.skysail.common.responses.SkysailResponse;
+import de.twenty11.skysail.server.ext.osgimonitor.BundleResource;
+import de.twenty11.skysail.server.ext.osgimonitor.HeaderResource;
+import de.twenty11.skysail.server.ext.osgimonitor.internal.OsgiMonitorViewerApplication;
+import org.codehaus.jackson.type.TypeReference;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Version;
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.representation.Representation;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class HeaderResourceTest extends BaseTests {
+    
+    private HeaderResource headerResource;
+    private Bundle bundle;
+
+	@Before
+    public void setUp() throws Exception {
+        
+        OsgiMonitorViewerApplication spy = setUpRestletApplication();
+        BundleContext bundleContext = mock(BundleContext.class);
+        bundle = mock(Bundle.class);
+        when(bundle.getBundleId()).thenReturn(99l);
+        when(bundle.getSymbolicName()).thenReturn("symbolic");
+        when(bundle.getLastModified()).thenReturn(111l);
+		when(bundleContext.getBundle(99l)).thenReturn(bundle);
+        when(bundle.getVersion()).thenReturn(new Version("1.0.0"));
+		when(spy.getBundleContext()).thenReturn(bundleContext);
+
+        Context context = mock(Context.class);
+        when(context.getAttributes()).thenReturn(new ConcurrentHashMap<String, Object>());
+
+        headerResource = new HeaderResource();
+        Request request = mock(Request.class);
+        ConcurrentMap<String, Object> attributes = new ConcurrentHashMap<String, Object>();
+        attributes.putIfAbsent("bundleId", "99");
+		when(request.getAttributes()).thenReturn(attributes);
+        headerResource.init(context, request, null);
+    }
+    
+    @Test
+    public void getHeader_returns_headerDetails_for_bundleId_from_request() throws Exception {
+    	SkysailResponse<HeaderDescriptor> bundle = headerResource.getHeader();
+    	//BundleDescriptor details = bundle.getData();
+    	//assertThat(details.getBundleId(), is(equalTo(99l)));
+    }
+    
+
+
+}
