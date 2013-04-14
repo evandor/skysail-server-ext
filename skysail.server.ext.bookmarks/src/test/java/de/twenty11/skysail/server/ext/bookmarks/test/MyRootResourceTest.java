d5662880289d8ee20c352797492da0a44de5004b
evandor <evandor@gmail.com>
2013-04-14 18:15:49 +0200

    wip

--- skysail.server.ext.bookmarks/src/test/java/de/twenty11/skysail/server/ext/bookmarks/test/MyRootResourceTest.java
+++ skysail.server.ext.bookmarks/src/test/java/de/twenty11/skysail/server/ext/bookmarks/test/MyRootResourceTest.java
@@ -1,13 +1,18 @@
 package de.twenty11.skysail.server.ext.bookmarks.test;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import de.twenty11.skysail.common.responses.SkysailResponse;
+import de.twenty11.skysail.common.selfdescription.ResourceDetails;
 import de.twenty11.skysail.server.ext.bookmarks.MyRootResource;
 
 import static org.junit.Assert.assertThat;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
 
@@ -23,6 +28,12 @@ public class MyRootResourceTest {
     @Test
     public void has_proper_name() {
         assertThat(rootResource.getName(), is(equalTo("bookmarks root resource")));
+    }
+
+    @Test
+    public void returns_methods() {
+        SkysailResponse<List<ResourceDetails>> methods = rootResource.getEntities();
+        assertThat(methods.getData().size(), is(greaterThan(1)));
     }
 
 }
