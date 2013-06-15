c32c4e3cadea8c1346d5fdf90aafa3e090640603
Carsten Gräf <evandor@gmail.com>
2013-06-15 08:30:49 +0200

    OPEN - issue SSEOM-1: Improve services html view http://jira.twentyeleven.de/browse/SSEOM-1

--- skysail.server.ext.osgimonitor/src/main/java/de/twenty11/skysail/server/ext/osgimonitor/resources/ServicesResource.java
+++ skysail.server.ext.osgimonitor/src/main/java/de/twenty11/skysail/server/ext/osgimonitor/resources/ServicesResource.java
@@ -10,15 +10,12 @@ import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
-import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ResourceException;
 
-import de.twenty11.skysail.common.ext.osgimonitor.RestfulServices;
 import de.twenty11.skysail.common.ext.osgimonitor.ServiceDescriptor;
-import de.twenty11.skysail.common.responses.SkysailResponse;
+import de.twenty11.skysail.server.core.restlet.ListServerResource2;
 import de.twenty11.skysail.server.ext.osgimonitor.OsgiMonitorViewerApplication;
-import de.twenty11.skysail.server.restlet.ListServerResource;
 
 /**
  * Restlet Resource class for handling OSGi Services.
@@ -28,51 +25,56 @@ import de.twenty11.skysail.server.restlet.ListServerResource;
  * The managed entity is of type {@link ServiceDescriptor}, providing details.
  * 
  */
-public class ServicesResource extends ListServerResource<ServiceDescriptor> implements RestfulServices {
+public class ServicesResource extends ListServerResource2<ServiceDescriptor> { // implements RestfulServices {
 
-	private List<ServiceReference> services = Collections.emptyList();
+    private List<ServiceReference> services = Collections.emptyList();
 
-	public ServicesResource() {
-		setName("osgimonitor bundles resource");
-		setDescription("The resource containing the list of bundles");
-	}
+    public ServicesResource() {
+        setName("osgimonitor bundles resource");
+        setDescription("The resource containing the list of bundles");
+    }
 
-	@Override
-	protected void doInit() throws ResourceException {
-		OsgiMonitorViewerApplication app = (OsgiMonitorViewerApplication) getApplication();
-		BundleContext bundleContext = app.getBundleContext();
-		if (bundleContext != null) {
-			try {
-				ServiceReference[] allServiceReferences = bundleContext.getAllServiceReferences(null, null);
-				services = Arrays.asList(allServiceReferences);
-			} catch (InvalidSyntaxException e) {
-				throw new ResourceException(e);
-			}
-		}
-	}
+    @Override
+    protected void doInit() throws ResourceException {
+        OsgiMonitorViewerApplication app = (OsgiMonitorViewerApplication) getApplication();
+        BundleContext bundleContext = app.getBundleContext();
+        if (bundleContext != null) {
+            try {
+                ServiceReference[] allServiceReferences = bundleContext.getAllServiceReferences(null, null);
+                services = Arrays.asList(allServiceReferences);
+            } catch (InvalidSyntaxException e) {
+                throw new ResourceException(e);
+            }
+        }
+    }
 
-	@Override
-	@Get("html|json")
-    public SkysailResponse<List<ServiceDescriptor>> getServices() {
-		return getEntities(allServices(), "all Services");
-	}
+    @Override
+    protected List<ServiceDescriptor> getData() {
+        return allServices();
+    }
+
+    // @Override
+    // @Get("html|json")
+    // public SkysailResponse<List<ServiceDescriptor>> getServices() {
+    // return getEntities(allServices(), "all Services");
+    // }
 
     @Post
-	public Representation install(String location) {
-		String prefix = "prefix";
-		if (!location.startsWith(prefix)) {
-			return new StringRepresentation("location didn't start with '" + prefix + "'");
-		}
-		return new StringRepresentation("success");
-	}
+    public Representation install(String location) {
+        String prefix = "prefix";
+        if (!location.startsWith(prefix)) {
+            return new StringRepresentation("location didn't start with '" + prefix + "'");
+        }
+        return new StringRepresentation("success");
+    }
 
     private List<ServiceDescriptor> allServices() {
         List<ServiceDescriptor> result = new ArrayList<ServiceDescriptor>();
-		for (ServiceReference sr : services) {
-			ServiceDescriptor descriptor = new ServiceDescriptor(sr, getReference());
-			result.add(descriptor);
-		}
-		return result;
-	}
+        for (ServiceReference sr : services) {
+            ServiceDescriptor descriptor = new ServiceDescriptor(sr, getReference());
+            result.add(descriptor);
+        }
+        return result;
+    }
 
 }
