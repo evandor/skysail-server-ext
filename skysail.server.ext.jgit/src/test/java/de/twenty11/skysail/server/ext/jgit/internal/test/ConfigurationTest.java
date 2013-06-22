f0ea7fe5bb3e1e709ce89c0a96902bda13601be1
Carsten Gräf <evandor@gmail.com>
2013-06-22 13:50:02 +0200

    wip

--- skysail.server.ext.jgit/src/test/java/de/twenty11/skysail/server/ext/jgit/internal/test/ConfigurationTest.java
+++ skysail.server.ext.jgit/src/test/java/de/twenty11/skysail/server/ext/jgit/internal/test/ConfigurationTest.java
@@ -1,32 +1,32 @@
-package de.twenty11.skysail.server.ext.jgit.internal.test;
-
-import javax.persistence.EntityManagerFactory;
-
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.restlet.Component;
-
-import de.twenty11.skysail.server.ext.jgit.internal.Configuration;
-import de.twenty11.skysail.server.services.ComponentProvider;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ConfigurationTest {
-
-    @Mock
-    private ComponentProvider componentProvider;
-
-    private Component component = new Component();
-
-    private Configuration configuration = new Configuration();
-
-    @Before
-    public void createConfiguration() throws Exception {
-        Mockito.when(componentProvider.getComponent()).thenReturn(component);
-        EntityManagerFactory emf = Mockito.mock(EntityManagerFactory.class);
-        configuration.setEmf(emf);
-    }
-
-}
+//package de.twenty11.skysail.server.ext.jgit.internal.test;
+//
+//import javax.persistence.EntityManagerFactory;
+//
+//import org.junit.Before;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.runners.MockitoJUnitRunner;
+//import org.restlet.Component;
+//
+//import de.twenty11.skysail.server.ext.jgit.internal.Configuration;
+//import de.twenty11.skysail.server.services.ComponentProvider;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class ConfigurationTest {
+//
+//    @Mock
+//    private ComponentProvider componentProvider;
+//
+//    private Component component = new Component();
+//
+//    private Configuration configuration = new Configuration();
+//
+//    @Before
+//    public void createConfiguration() throws Exception {
+//        Mockito.when(componentProvider.getComponent()).thenReturn(component);
+//        EntityManagerFactory emf = Mockito.mock(EntityManagerFactory.class);
+//        configuration.setEmf(emf);
+//    }
+//
+//}
