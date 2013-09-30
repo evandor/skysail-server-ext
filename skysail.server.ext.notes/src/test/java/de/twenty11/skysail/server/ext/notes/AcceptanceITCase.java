063001b5093554f2986e29e80c40107d67f3ae61
Carsten Gräf <evandor@gmail.com>
2013-09-30 07:42:08 +0200

    enabling acceptance tests again

--- skysail.server.ext.notes/src/test/java/de/twenty11/skysail/server/ext/notes/AcceptanceITCase.java
+++ skysail.server.ext.notes/src/test/java/de/twenty11/skysail/server/ext/notes/AcceptanceITCase.java
@@ -6,7 +6,7 @@ public class AcceptanceITCase extends ThucydidesJUnitStories {
     public AcceptanceITCase() {
         // findStoriesCalled("**/adding_folder/*.story");
         // findStoriesCalled("**/requesting_folder/*.story");
-        findStoriesCalled("**/altering_folder/*.story");
+        // findStoriesCalled("**/altering_folder/*.story");
         // findStoriesCalled("folder_management/**/*.story");
     }
 }
